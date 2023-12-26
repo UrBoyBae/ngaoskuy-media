343385f26c8d9e1a7917ded3c451b48f9159fd4b
UrBoyBae <aliakbarabdillah21@gmail.com>
2023-12-26 19:54:13 +0700

    setup frontend requirements

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ngaos Kuy!</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="icon" href="{{ asset('assets/images/faviconNgaosKuy.png') }}" type="image/x-icon">
+
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+    <div class="w-full">
+        <h1 class="text-3xl font-bold underline text-red-500">
+            Hello world!
+        </h1>
+    </div>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+
+</html>
