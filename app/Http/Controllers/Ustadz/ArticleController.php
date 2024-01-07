f31c81592e8ef12cf089c31030311210c3bfb44f
HitMagicc <yaar81@gmail.com>
2024-01-07 23:51:49 +0700

    arya, feature/UstadzController

---
+++ app/Http/Controllers/Ustadz/ArticleController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Ustadz;
+
+use App\Http\Controllers\Controller;
+use App\Models\Article;
+use App\Models\Episode;
+use App\Models\Kitab;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    public function index(Request $request)
+    {
+        $user = auth()->user();
+        $episode = Episode::all();
+        $kitab = Kitab::with(['bab', 'subbab', 'judul', 'episode'])->get();
+        $article  = Article::all()->sortBy('created_at');
+        return [
+            'title' => 'Article',
+            'user' => $user,
+            'episode' => $episode,
+            'kitab' => $kitab,
+            'article' => $article,
+        ];
+    }
+
+    public function show($id)
+    {
+        $user = auth()->user();
+        $article  = Article::where('id', $id)->first();
+        return [
+            'title' => $article->name,
+            'user' => $user,
+            'article' => $article,
+        ];
+    }
+
+    public function create(Request $request)
+    {
+        $user = auth()->user();
+        $request->validate([
+            'name' => 'required',
+            'content' => 'required',
+        ]);
+        Article::create([
+            'name' => $request->name,
+            'content' => $request->content,
+        ]);
+
+        return to_route('namarute')->with('success', 'Article Successfuly Created');
+    }
+    public function edit($id)
+    {
+        $user = auth()->user();
+        $article = Article::findOrFail($id);
+        return [
+            'title' => 'Article Edit',
+            'user' => $user,
+            'article' => $article
+        ];
+    }
+    public function update(Request $request, $id)
+    {
+        $article = Article::findOrFail($id);
+        $article->update([
+            'name' => $request->name,
+            'content' => $request->content,
+        ]);
+        return to_route('namarute')->with('success', 'Article Successfuly Edited');
+    }
+
+    public function delete($id)
+    {
+        $article = Article::findOrFail($id);
+        $article->delete();
+        return to_route('namarute')->with('success', 'Article Successfuly Deleted');
+    }
+}
---
+++ app/Http/Controllers/Ustadz/DashboardController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Ustadz;
+
+use App\Http\Controllers\Controller;
+use App\Models\Article;
+use App\Models\Episode;
+use App\Models\Kitab;
+use App\Models\Question;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $user = auth()->user();
+        $episode = Episode::all();
+        $question = Question::all();
+        $kitab = Kitab::with(['bab', 'subbab', 'judul', 'episode'])->get();
+        $article  = Article::all();
+        return [
+            'title' => 'Home',
+            'user' => $user,
+            'episode' => $episode,
+            'question' => $question,
+            'kitab' => $kitab,
+            'article' => $article,
+        ];
+    }
+}
