ede38316ef7aaf00f3ebf8065c40b1252b22a789
unknown <yaar81@gmail.com>
2024-01-13 21:59:06 +0700

    arya, hotfix/Deleted-unused-admin-and-user-folder

--- resources/views/admin/dashboard.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.index')
-
-@section('mainContent')
-    <h1>INI ADMIN</h1>
-@endsection
--- resources/views/user/question.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.index')
-
-@section('mainContent')
-    <H5>INI USER/Question</H5>
-    <h1>{{ $question->subject }}</h1>
-@endsection
