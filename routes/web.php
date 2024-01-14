cd652382a2f311d3ba30a4bb021fd2466b7a6657
unknown <yaar81@gmail.com>
2024-01-14 10:24:47 +0700

    arya, hotfix/route-name-fixed

--- routes/web.php
+++ routes/web.php
@@ -84,38 +84,38 @@ Route::get('/member/video-show/{param}', [MemberVideoListController::class, 'sho
 //admin routes
 Route::middleware(['auth', 'user-role:admin'])->group(function () {
 
-    Route::get('/admin/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard.index');
-    Route::get('/admin/dashboard/create-kitab', [AdminDashboardController::class, 'createKitab'])->name('admin.dashboard.create-kitab');
-    Route::post('/admin/dashboard/store-kitab', [AdminDashboardController::class, 'storeKitab'])->name('admin.dashboard.store-kitab');
-    Route::get('/admin/dashboard/edit-kitab/{param}', [AdminDashboardController::class, 'editKitab'])->name('admin.dashboard.edit-kitab');
-    Route::post('/admin/dashboard/update-kitab/{param}', [AdminDashboardController::class, 'updateKitab'])->name('admin.dashboard.update-kitab');
-    Route::post('/admin/dashboard/delete-kitab/{param}', [AdminDashboardController::class, 'deleteKitab'])->name('admin.dashboard.delete-kitab');
+    Route::get('/admin/dashboard', [AdminDashboardController::class, 'index'])->name('admin.home.index');
+    Route::get('/admin/dashboard/create-kitab', [AdminDashboardController::class, 'createKitab'])->name('admin.home.create-kitab');
+    Route::post('/admin/dashboard/store-kitab', [AdminDashboardController::class, 'storeKitab'])->name('admin.home.store-kitab');
+    Route::get('/admin/dashboard/edit-kitab/{param}', [AdminDashboardController::class, 'editKitab'])->name('admin.home.edit-kitab');
+    Route::post('/admin/dashboard/edit-kitab/{param}', [AdminDashboardController::class, 'updateKitab'])->name('admin.home.update-kitab');
+    Route::post('/admin/dashboard/delete-kitab/{param}', [AdminDashboardController::class, 'deleteKitab'])->name('admin.home.delete-kitab');
 
-    Route::get('/admin/dashboard/create-Bab', [AdminDashboardController::class, 'createBab'])->name('admin.dashboard.create-bab');
-    Route::post('/admin/dashboard/store-Bab', [AdminDashboardController::class, 'storeBab'])->name('admin.dashboard.store-bab');
-    Route::get('/admin/dashboard/edit-Bab/{param}', [AdminDashboardController::class, 'editBab'])->name('admin.dashboard.edit-bab');
-    Route::post('/admin/dashboard/update-Bab/{param}', [AdminDashboardController::class, 'updateBab'])->name('admin.dashboard.update-bab');
-    Route::post('/admin/dashboard/delete-Bab/{param}', [AdminDashboardController::class, 'deleteBab'])->name('admin.dashboard.delete-bab');
+    Route::get('/admin/dashboard/create-Bab', [AdminDashboardController::class, 'createBab'])->name('admin.home.create-bab');
+    Route::post('/admin/dashboard/store-Bab', [AdminDashboardController::class, 'storeBab'])->name('admin.home.store-bab');
+    Route::get('/admin/dashboard/edit-Bab/{param}', [AdminDashboardController::class, 'editBab'])->name('admin.home.edit-bab');
+    Route::post('/admin/dashboard/edit-Bab/{param}', [AdminDashboardController::class, 'updateBab'])->name('admin.home.update-bab');
+    Route::post('/admin/dashboard/delete-Bab/{param}', [AdminDashboardController::class, 'deleteBab'])->name('admin.home.delete-bab');
 
     Route::get('/admin/bab/{param}', [BabController::class, 'index'])->name('admin.bab.index');
     Route::get('/admin/bab/create-subbab', [BabController::class, 'createSubbab'])->name('admin.bab.create-subbab');
     Route::post('/admin/bab/store-subbab/{param}', [BabController::class, 'storeSubbab'])->name('admin.bab.store-subbab');
     Route::get('/admin/bab/edit-subbab/{param}', [BabController::class, 'editSubbab'])->name('admin.bab.edit-subbab');
-    Route::post('/admin/bab/update-subbab/{param}', [BabController::class, 'updateSubbab'])->name('admin.bab.update-subbab');
+    Route::post('/admin/bab/edit-subbab/{param}', [BabController::class, 'updateSubbab'])->name('admin.bab.update-subbab');
     Route::post('/admin/bab/delete-subbab/{param}', [BabController::class, 'deleteSubbab'])->name('admin.bab.delete-subbab');
 
     Route::get('/admin/subbab/{param}', [SubBabController::class, 'index'])->name('admin.subbab.index');
     Route::get('/admin/subbab/create-judul', [SubBabController::class, 'createJudul'])->name('admin.subbab.create-judul');
     Route::post('/admin/subbab/store-judul/{param}', [SubBabController::class, 'storeJudul'])->name('admin.subbab.store-judul');
     Route::get('/admin/subbab/edit-judul/{param}', [SubBabController::class, 'editJudul'])->name('admin.subbab.edit-judul');
-    Route::post('/admin/subbab/update-judul/{param}', [SubBabController::class, 'updateJudul'])->name('admin.subbab.update-judul');
+    Route::post('/admin/subbab/edit-judul/{param}', [SubBabController::class, 'updateJudul'])->name('admin.subbab.update-judul');
     Route::post('/admin/subbab/delete-judul/{param}', [SubBabController::class, 'deleteJudul'])->name('admin.subbab.delete-judul');
 
     Route::get('/admin/episode/{param}', [EpisodeController::class, 'index'])->name('admin.episode.index');
     Route::get('/admin/episode/create-episode', [EpisodeController::class, 'createEpisode'])->name('admin.episode.create-episode');
     Route::post('/admin/episode/store-episode/{param}', [EpisodeController::class, 'storeEpisode'])->name('admin.episode.store-episode');
     Route::get('/admin/episode/edit-episode/{param}', [EpisodeController::class, 'editEpisode'])->name('admin.episode.edit-judul');
-    Route::post('/admin/episode/update-episode/{param}', [EpisodeController::class, 'updateEpisode'])->name('admin.episode.update-episode');
+    Route::post('/admin/episode/edit-episode/{param}', [EpisodeController::class, 'updateEpisode'])->name('admin.episode.update-episode');
     Route::post('/admin/episode/delete-episode/{param}', [EpisodeController::class, 'deleteEpisode'])->name('admin.episode.delete-episode');
 
     Route::get('/admin/artikel', [AdminArticleController::class, 'index'])->name('admin.article.index');
@@ -123,7 +123,7 @@ Route::middleware(['auth', 'user-role:admin'])->group(function () {
     Route::get('/admin/artikel/create', [AdminArticleController::class, 'create'])->name('admin.article.create');
     Route::post('/admin/artikel/store', [AdminArticleController::class, 'store'])->name('admin.article.store');
     Route::get('/admin/artikel/edit/{param}', [AdminArticleController::class, 'edit'])->name('admin.article.edit');
-    Route::post('/admin/artikel/update/{param}', [AdminArticleController::class, 'update'])->name('admin.article.update');
+    Route::post('/admin/artikel/edit/{param}', [AdminArticleController::class, 'update'])->name('admin.article.update');
     Route::post('/admin/artikel/delete/{param}', [AdminArticleController::class, 'delete'])->name('admin.article.delete');
 });
 
@@ -138,12 +138,12 @@ Route::get('/ustadz/artikel/{param}', [UstadzArticleController::class, 'show'])-
 Route::get('/ustadz/artikel/create', [UstadzArticleController::class, 'create'])->name('ustadz.article.create');
 Route::post('/ustadz/artikel/store', [UstadzArticleController::class, 'store'])->name('ustadz.article.store');
 Route::get('/ustadz/artikel/edit/{param}', [UstadzArticleController::class, 'edit'])->name('ustadz.article.edit');
-Route::post('/ustadz/artikel/update/{param}', [UstadzArticleController::class, 'update'])->name('ustadz.article.update');
+Route::post('/ustadz/artikel/edit/{param}', [UstadzArticleController::class, 'update'])->name('ustadz.article.update');
 Route::post('/ustadz/artikel/delete/{param}', [UstadzArticleController::class, 'delete'])->name('ustadz.article.delete');
 
-Route::get('/ustadz/pertanyaan', [PertanyaanController::class, 'index'])->name('ustadz.pertnyaan.index');
-Route::get('/ustadz/pertanyaan/{param}', [PertanyaanController::class, 'show'])->name('ustadz.pertnyaan.show');
-Route::post('/ustadz/pertanyaan/post/{param}', [PertanyaanController::class, 'store'])->name('ustadz.pertnyaan.store');
+Route::get('/ustadz/question', [PertanyaanController::class, 'index'])->name('ustadz.question.index');
+Route::get('/ustadz/question/{param}', [PertanyaanController::class, 'show'])->name('ustadz.question.show');
+Route::post('/ustadz/question/{param}', [PertanyaanController::class, 'store'])->name('ustadz.question.store');
 
 Route::get('/ustadz/video/{param}', [VideoController::class, 'index'])->name('ustadz.video.index');
 Route::get('/ustadz/video-show/{param}', [VideoController::class, 'show'])->name('ustadz.video.show');
