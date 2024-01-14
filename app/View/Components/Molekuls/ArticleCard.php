afe1d9b5d2cf9f49b52cebd1e6cea0d079332e96
UrBoyBae <aliakbarabdillah21@gmail.com>
2024-01-14 09:58:18 +0700

    ali, features/update-user-dashboard

--- app/View/Components/Molekuls/ArticleCard.php
+++ app/View/Components/Molekuls/ArticleCard.php
@@ -12,14 +12,20 @@ class ArticleCard extends Component
      * Create a new component instance.
      */
     public string $id;
-    public string $subject;
-    public string $date;
+    public string $name;
+    public string $content;
+    public string $created_at;
+    public string $updated_at;
+    public string $route;
     
-    public function __construct($data)
+    public function __construct($data, $route)
     {
-        $this->id = $data->id;
-        $this->subject = $data->subject;
-        $this->date = $data->date;
+        $this->id = isset($data->id)?$data->id:"";
+        $this->name = isset($data->name)?$data->name:"";
+        $this->content = isset($data->content)?$data->content:"";
+        $this->created_at = isset($data->created_at)?$data->created_at:"";
+        $this->updated_at = isset($data->updated_at)?$data->updated_at:"";
+        $this->route = isset($route)?$route:"";
     }
 
     /**
--- resources/views/components/molekuls/article-card.blade.php
+++ resources/views/components/molekuls/article-card.blade.php
@@ -1,9 +1,12 @@
-<a href="">
+@php
+    $date = explode(' ', $created_at);
+@endphp
+<a href="{{ route("$route", $id) }}">
     <div
         class="min-w-[300px] max-w-[300px] h-[150px] rounded-xl border-2 border-[#CBCDB7] shadow-[-5px_5px_0_0_rgba(130,0,0,1)] snap-center px-5 py-3 flex flex-col gap-3">
-        <span class="text-lg font-semibold text-black line-clamp-3">{{ $subject }}</span>
+        <span class="text-lg font-semibold text-black line-clamp-3">{{ $content }}</span>
         <div class="flex items-center justify-between">
-            <p class="text-base font-semibold text-black opacity-50">{{ $date }}</p>
+            <p class="text-base font-semibold text-black opacity-50">{{ $date[0] }}</p>
             <ion-icon name="chevron-forward-outline" class="text-3xl"></ion-icon>
         </div>
     </div>
