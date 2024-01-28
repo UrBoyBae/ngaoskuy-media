4080f161649176f20ad7b19b989218aa40e185af
UrBoyBae <aliakbarabdillah21@gmail.com>
2024-01-28 19:48:40 +0700

    ali, hotfix/update-route-all-page

--- resources/views/components/templates/user/dashboard/index.blade.php
+++ resources/views/components/templates/user/dashboard/index.blade.php
@@ -1,12 +1,15 @@
 @extends('layouts.index')
 
 @section('mainContent')
+    @php
+        $role = empty($roles) ? 'user' : $roles[0];
+    @endphp
     <div class="w-full pt-9 bg-[#EEEBDD]">
         @include('components.organisme.episode-slider')
         <div class="w-full mt-5 md:mt-10">
             <div class="flex items-center justify-between pl-8 pr-8 md:pr-12 md:pl-12">
                 <span class="text-2xl font-bold text-black">Pertanyaan</span>
-                <a href="{{ route("user.question.index") }}">
+                <a href="{{ route($role . '.question.index') }}">
                     <div class="flex justify-center items-center gap-1">
                         <span class="text-base font-semibold md:text-xl">See All</span>
                         <ion-icon name="chevron-forward-outline" class="text-base md:text-xl"></ion-icon>
@@ -16,14 +19,14 @@
             <div class="w-full min-h-[190px] mt-3 flex gap-5 overflow-x-auto snap-mandatory snap-x custom-x-scrollbar px-5">
                 {{-- Masukkan variabel yang menampung data pertanyaan kedalam foreach --}}
                 @foreach ($question as $data)
-                    <x-molekuls.question-card :data="$data" route="user.question.show" width="300px"/>
+                    <x-molekuls.question-card :data="$data" route="{{ $role }}.question.show" width="300px" />
                 @endforeach
             </div>
         </div>
         <div class="w-full mt-5 md:mt-5">
             <div class="flex items-center justify-between pl-8 pr-8 md:pr-12 md:pl-12">
                 <span class="text-2xl font-bold text-black">Artikel</span>
-                <a href="{{ route("user.article.index") }}">
+                <a href="{{ route($role . '.article.index') }}">
                     <div class="flex justify-center items-center gap-1">
                         <span class="text-base font-semibold md:text-xl">See All</span>
                         <ion-icon name="chevron-forward-outline" class="text-base md:text-xl"></ion-icon>
@@ -33,7 +36,7 @@
             {{-- Masukkan variabel yang menampung data artikel kedalam foreach --}}
             <div class="w-full min-h-[190px] mt-3 flex gap-5 overflow-x-auto snap-mandatory snap-x custom-x-scrollbar px-5">
                 @foreach ($article as $data)
-                    <x-molekuls.article-card :data="$data" route="user.article.show"/>
+                    <x-molekuls.article-card :data="$data" route="{{ $role }}.article.show" />
                 @endforeach
             </div>
 
--- resources/views/components/templates/user/video/show.blade.php
+++ resources/views/components/templates/user/video/show.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.index')
 
 @section('mainContent')
+    @php
+        $role = empty($roles) ? 'user' : $roles[0];
+    @endphp
     <div class="w-full pt-8 pb-12 bg-[#EEEBDD] px-7 lg:px-12 flex flex-col lg:flex-row gap-8">
         <div class="w-full flex flex-col gap-5">
             <iframe class="w-full aspect-video rounded-xl" src="https://www.youtube.com/embed/{{ $episode->video_link }}"
@@ -17,7 +20,7 @@
         </div>
         <div class="w-fit grid grid-cols-1 md:grid-cols-2 lg:flex lg:flex-col gap-5 justify-items-stretch">
             @foreach ($episodelist as $data)
-                <x-molekuls.video-display :data="$data" route="user.video.display" />
+                <x-molekuls.video-display :data="$data" route="{{ $role }}.video.display" />
             @endforeach
         </div>
     </div>
