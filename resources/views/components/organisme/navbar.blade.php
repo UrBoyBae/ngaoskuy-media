83bd5b60b4ad3c57ad42ef6a795812c02a1ec165
UrBoyBae <aliakbarabdillah21@gmail.com>
2024-01-28 19:31:58 +0700

    ali, features/create-modal-setting-profile

--- resources/views/components/organisme/navbar.blade.php
+++ resources/views/components/organisme/navbar.blade.php
@@ -1,5 +1,5 @@
 @php
-    $role = empty($roles) ? "user" : $roles[0];
+    $role = empty($roles) ? 'user' : $roles[0];
 @endphp
 
 <div class="w-full py-3 px-5 bg-[#810000] flex justify-between items-center">
@@ -40,7 +40,7 @@
                             <ul class="space-y-6 border-l-2 border-slate-300 ml-1">
                                 @foreach ($k->bab as $bab)
                                     <li>
-                                        <a href="{{ route($role.'.video.index', $bab->id) }}"
+                                        <a href="{{ route($role . '.video.index', $bab->id) }}"
                                             class="block border-l-2 pl-4 -ml-[2px] text-[#810000] border-current font-semibold">{{ $bab->name }}</a>
                                     </li>
                                 @endforeach
@@ -51,10 +51,10 @@
             </div>
         </div>
     </div>
-    <a href="{{ route($role.'.home.index') }}" class="text-lg font-bold text-white md:text-xl">NGAOS KUY</a>
+    <a href="{{ route($role . '.home.index') }}" class="text-lg font-bold text-white md:text-xl">NGAOS KUY</a>
     @if (Auth::check())
-        <img src="{{ asset('assets/images/kobo.jpg') }}" alt="profilePict"
-            class="w-8 h-8 rounded-full cursor-pointer" id="toggle-navbar-profile">
+        <img src="{{ asset('assets/images/kobo.jpg') }}" alt="profilePict" class="w-8 h-8 rounded-full cursor-pointer"
+            id="toggle-navbar-profile">
     @else
         <img src="{{ asset('assets/images/user.png') }}" alt="profilePict" class="w-8 h-8 rounded-full cursor-pointer"
             id="toggle-navbar-profile">
@@ -93,7 +93,7 @@
                     <span class="text-base font-bold text-[#d6c3c3]/50"></span>
                 </div>
                 <div class="w-full rounded-[30px] bg-[#942626] mt-3">
-                    <a href="{{ route("login.index") }}">
+                    <a href="{{ route('login.index') }}">
                         <div
                             class="flex items-center cursor-pointer px-6 py-4 gap-3 hover:bg-[#d6c3c3]/10 hover:rounded-[30px] w-full">
                             <ion-icon name="log-out-outline" class="text-xl font-bold text-white"></ion-icon>
@@ -105,6 +105,48 @@
 
         </div>
 
+    </div>
+    <div class="hidden z-50 inset-0 px-4 pt-4"
+        id="main-setting-profile">
+        <div class="fixed inset-0 bg-black/30 backdrop-blur-sm"></div>
+        <div class="relative bg-[#810000] m-auto w-full max-w-[800px] h-fit rounded-[30px] pt-6 md:pt-12 pb-7 px-6 md:pr-16">
+            <form method="" action="" id="update-profile">
+                <div class="flex flex-col gap-5 md:flex-row">
+                    <div class="flex flex-col items-center gap-2 md:w-2/6">
+                        <img src="{{ asset('assets/images/kobo.jpg') }}" alt="profilePict"
+                            class="w-28 h-28 md:w-32 md:h-32 rounded-full" id="image-setting-profile">
+                        <span class="underline text-lg font-semibold text-white">Edit Profile</span>
+                    </div>
+                    <div class="bg-[#942626] rounded-3xl px-7 md:px-10 py-5 md:py-8 flex flex-col gap-4 md:w-4/6">
+                        <div class="flex flex-col gap-1">
+                            <label class="text-white text-lg font-semibold" for="nama">Nama</label>
+                            <input class="bg-transparent border border-[#d6c3c380] rounded-md w-full h-full text-lg focus:outline-none placeholder-[rgba(214,195,195,0.5)] text-[rgba(214,195,195,0.5)] font-semibold pl-2 py-2" value="Pasha Adelia" type="text" name="nama" id="nama">
+                        </div>
+                        <div class="flex flex-col gap-1">
+                            <label class="text-white text-lg font-semibold" for="username">Username</label>
+                            <input class="bg-transparent border border-[#d6c3c380] rounded-md w-full h-full text-lg focus:outline-none placeholder-[rgba(214,195,195,0.5)] text-[rgba(214,195,195,0.5)] font-semibold pl-2 py-2" value="Pasha Adelia" type="text" name="username" id="username">
+                        </div>
+                        <div class="flex flex-col gap-1">
+                            <label class="text-white text-lg font-semibold" for="email">Email</label>
+                            <input class="bg-transparent border border-[#d6c3c380] rounded-md w-full h-full text-lg focus:outline-none placeholder-[rgba(214,195,195,0.5)] text-[rgba(214,195,195,0.5)] font-semibold pl-2 py-2" value="Pasha Adelia" type="email" name="email" id="email">
+                        </div>
+                        <div class="flex flex-col gap-1">
+                            <label class="text-white text-lg font-semibold" for="password">Password</label>
+                            <input class="bg-transparent border border-[#d6c3c380] rounded-md w-full h-full text-lg focus:outline-none placeholder-[rgba(214,195,195,0.5)] text-[rgba(214,195,195,0.5)] font-semibold pl-2 py-2" value="Pasha Adelia" type="password" name="password" id="password">
+                        </div>
+                        <div class="flex flex-col gap-1">
+                            <label class="text-white text-lg font-semibold" for="confirm-password">Confirm Password</label>
+                            <input class="bg-transparent border border-[#d6c3c380] rounded-md w-full h-full text-lg focus:outline-none placeholder-[rgba(214,195,195,0.5)] text-[rgba(214,195,195,0.5)] font-semibold pl-2 py-2" value="Pasha Adelia" type="password" name="confirm-password" id="confirm-password">
+                        </div>
+                        <button class="bg-[rgba(27,23,23,0.7)] py-2 w-28 rounded-3xl text-white font-semibold mt-3" id="change-data-profile">CHANGE</button>
+                    </div>
+                </div>
+                <div class="flex gap-5 justify-end mt-6">
+                    <button class="bg-[rgba(27,23,23,0.7)] py-2 w-28 rounded-3xl text-white font-semibold" id="close-main-setting-profile">BACK</button>
+                    <button class="bg-[rgba(27,23,23,0.7)] py-2 w-28 rounded-3xl text-white font-semibold" type="submit">SAVE</button>
+                </div>
+            </form>
+        </div>
     </div>
 </div>
 <div class="w-full h-[21px] bg-[#CEA59B]"></div>
--- resources/views/components/templates/member/dashboard/index.blade.php
+++ resources/views/components/templates/member/dashboard/index.blade.php
@@ -30,7 +30,7 @@
         <div class="w-full mt-5 md:mt-5">
             <div class="flex items-center justify-between pl-8 pr-8 md:pr-12 md:pl-12">
                 <span class="text-2xl font-bold text-black">Artikel</span>
-                <a href="{{ route("member.artikel.index") }}">
+                <a href="{{ route("member.article.index") }}">
                     <div class="flex justify-center items-center gap-1">
                         <span class="text-base font-semibold md:text-xl">See All</span>
                         <ion-icon name="chevron-forward-outline" class="text-base md:text-xl"></ion-icon>
@@ -38,9 +38,9 @@
                 </a>
             </div>
             {{-- Masukkan variabel yang menampung data artikel kedalam foreach --}}
-            <div class="w-full min-h-[170px] mt-3 flex gap-5 overflow-x-auto snap-mandatory snap-x custom-x-scrollbar px-5">
+            <div class="w-full min-h-[190px] mt-3 flex gap-5 overflow-x-auto snap-mandatory snap-x custom-x-scrollbar px-5">
                 @foreach ($article as $data)
-                    <x-molekuls.article-card :data="$data" route="member.artikel.show"/>
+                    <x-molekuls.article-card :data="$data" route="member.article.show"/>
                 @endforeach
             </div>
 
